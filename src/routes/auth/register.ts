0ae2b9d98fcf7338dd5c16a78393bc8c0a1c11fb
Josiah Eakle <josiaheakle@gmail.com>
2021-05-02 23:10:57 -0400

    Form changes.

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,12 +1,48 @@
 import * as Express from "express";
 import { body, validationResult } from "express-validator";
 import { Model } from "../../classes/Model";
+import { Form } from "../../classes/Form";
 
 const router = Express.Router();
 
+const registerForm = new Form([
+    {
+        id: 'first-name-input',
+        name: 'firstName',
+        type: 'text',
+        displayString: 'First Name'
+    },
+    {
+        id: 'last-name-input',
+        name: 'lastName',
+        type: 'text',
+        displayString: 'Last Name'
+    },
+    {
+        id: 'email-input',
+        name: 'email',
+        type: 'text',
+        displayString: 'Email'
+    },
+    {
+        id : 'password-input',
+        name: 'password',
+        type: 'password',
+        displayString : 'Password'
+    },
+    {
+        id : 'password-confirm-input',
+        name: 'passwordConfirm',
+        type: 'password',
+        displayString : 'Repeat Password'
+    }
+]);
+
 router.get('/', (req : Express.Request, res : Express.Response, next : Function) => {
-    res.render('register', {
-        layout: 'nouser'
+    res.render('reg', {
+        layout: 'nouser',
+        inputProperties : registerForm.inputs
+
     });
 });
 
@@ -23,31 +59,31 @@ router.post('/',
     }),
     (req : Express.Request, res : Express.Response, next : Function) => {
 
-        const inputValues = {
-            firstName : req.body.firstName ?? '', 
-            lastName : req.body.lastName ?? '',
-            email :  req.body.email ?? '',
-            password : req.body.password ?? ''
-        }
-
         const valErrors = validationResult(req);
-        console.log({errors : valErrors['errors']});
+
+        registerForm.updateValues(req);
+        registerForm.updateErrors(valErrors['errors']);
+
+        console.log({['registration inputs'] : registerForm.inputs})
+        registerForm.inputs.forEach(i => {
+            console.log(i.errors);
+        });
+
         if(!valErrors.isEmpty()) {
-            res.render('register', {
+            res.render('reg', {
                 layout : 'nouser',
-                validationErrors: valErrors['errors'],
-                values : inputValues
+                inputProperties : registerForm.inputs
             });
         }
 
-        const registerModel = new Model('users', [{ name : 'firstName' }, {name:'lastName'}, {name: 'email'}, {name : "password"}]);
-        registerModel.loadBody(req);
-        registerModel.save();
+        // const registerModel = new Model('users', [{ name : 'firstName' }, {name:'lastName'}, {name: 'email'}, {name : "password"}]);
+        // registerModel.loadBody(req);
+        // registerModel.save();
 
-        res.writeHead(301,
-            {Location: 'http://localhost:4000/'}
-        );
-        res.end();        
+        // res.writeHead(301,
+        //     {Location: 'http://localhost:4000/'}
+        // );
+        // res.end();        
 
     }
 );
