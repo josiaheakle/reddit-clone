1c07ca71755ce74f4ed2cf129817c72ab3cf41d9
Josiah Eakle <josiaheakle@gmail.com>
2021-05-05 15:44:16 -0400

    Adding user route for account management.

---
+++ src/routes/user.ts
@@ -0,0 +1,32 @@
+import * as Express from "express";
+const router = Express.Router();
+
+router.get('/', (req : Express.Request, res : Express.Response, next: Function) => {
+
+    res.render('user', {
+        user: req.session.user
+    });
+
+});
+
+router.get('/logout', (req : Express.Request, res : Express.Response, next: Function) => {
+
+    req.session.destroy((err) => {
+        if (err) console.error(err);
+        else res.redirect('/');
+    });
+
+});
+
+router.get('/delete', (req : Express.Request, res : Express.Response, next: Function) => {
+
+    req.session.destroy((err) => {
+        if (err) console.error(err);
+        else res.redirect('/');
+    });
+
+});
+
+export {
+    router
+}
